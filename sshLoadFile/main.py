4d3f6393910b239cf616d318f62a56935ea6a85f
AstroKen <eaukotaro@icloud.com>
2024-05-20 10:06:18 +0900

    modified main.py

--- sshLoadFile/main.py
+++ sshLoadFile/main.py
@@ -2,19 +2,20 @@ import numpy as np
 import paramiko
 
 class sshLoadFile:
-    def __init__(self, hostname, usename, key_filename):
+    def __init__(self, hostname, usename, port, key_filename):
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.client.connect(
                 hostname=hostname,
                 username=usename,
+                port=port,
                 key_filename=key_filename
                 )
         self.sftp = self.client.open_sftp()
 
-    def sshloadtxt(self, filename):
+    def sshloadtxt(self, filename, nskip):
         with self.sftp.open(filename, 'r') as f:
-            data = np.loadtxt(f)
+            data = np.loadtxt(f, skiprows=nskip)
         return data
 
     def __del__(self):
